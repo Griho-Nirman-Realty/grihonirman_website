f1cec629e89c8502ce17c89cb3224c93c9fd91e9
Sourav Bhunia <bhuniasourav11@gmail.com>
2024-08-09 14:00:23 +0530

    home meta data add

--- index.php
+++ index.php
@@ -46,11 +46,28 @@ $system_ph_num = $system_info_data[5];
     } else {
     ?>
 
-        <meta name="description" content="Discover your dream home with Griho Nirman Realty Pvt Ltd, offering the best real estate properties, apartments, villas, and plots in Kolkata. Our multi-channel marketing ensures quality and transparency." />
-        <meta name="keywords" content="real estate, Realtor, best realtors, Zillow, housing market, homes for sale, properties for sale, real estate developers in Kolkata, plots for sale, apartments, villas, Griho Nirman Realty, home buying" />
-        <meta name="author" content="Griho Nirman Realty Pvt Ltd" />
+        <!-- HTML Meta Tags -->
+        <title>Welcome to Griho Nirman Realty Pvt Ltd</title>
+        <meta name="description" content="Welcome to Griho Nirman Realty Pvt Ltd. Discover premium real estate opportunities in Kolkata with our comprehensive property listings, expert services, and commitment to excellence. Find your dream home or investment property today.">
 
-        <link rel="canonical" href="https://grihonirmanrealty.in" />
+        <!-- Google / Search Engine Tags -->
+        <meta itemprop="name" content="Welcome to Griho Nirman Realty Pvt Ltd">
+        <meta itemprop="description" content="Welcome to Griho Nirman Realty Pvt Ltd. Discover premium real estate opportunities in Kolkata with our comprehensive property listings, expert services, and commitment to excellence. Find your dream home or investment property today.">
+        <meta itemprop="image" content="">
+
+        <!-- Facebook Meta Tags -->
+        <meta property="og:url" content="https://grihonirmanrealty.in/home">
+        <meta property="og:type" content="website">
+        <meta property="og:title" content="Welcome to Griho Nirman Realty Pvt Ltd">
+        <meta property="og:description" content="Welcome to Griho Nirman Realty Pvt Ltd. Discover premium real estate opportunities in Kolkata with our comprehensive property listings, expert services, and commitment to excellence. Find your dream home or investment property today.">
+        <meta property="og:image" content="upload_content/upload_img/system_img/<?php echo $system_favicon; ?>">
+
+        <!-- Twitter Meta Tags -->
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:title" content="Welcome to Griho Nirman Realty Pvt Ltd">
+        <meta name="twitter:description" content="Welcome to Griho Nirman Realty Pvt Ltd. Discover premium real estate opportunities in Kolkata with our comprehensive property listings, expert services, and commitment to excellence. Find your dream home or investment property today.">
+        <meta name="twitter:image" content="upload_content/upload_img/system_img/<?php echo $system_favicon; ?>">
+
     <?php
 
     }
