2fb68a933c333e4c05ac90bbec8f6f0588a65b8b
Bhuniasoruav <bhuniasoruav11@gmail.com>
2024-08-23 10:42:45 +0530

    convicity gallery page

--- templates/gallery/gallery.php
+++ templates/gallery/gallery.php
@@ -20,10 +20,10 @@ Breadcumb
 <!-- Gallery section -->
 <div class="process-area-3 overflow-hidden">
     <div class="service-shape1_1 shape-mockup jump-reverse d-lg-block d-none" data-top="0" data-right="0">
-        <img loading="lazy"  src="assets/img/service/service-shape1-1.png" alt="img">
+        <img loading="lazy"  src="assets/img/service/service-shape1-1.webp" alt="img">
     </div>
     <div class="service-shape1_2 shape-mockup jump-reverse d-lg-block d-none" data-top="0" data-left="0">
-        <img loading="lazy"  src="assets/img/service/service-shape1-2.png" alt="img">
+        <img loading="lazy"  src="assets/img/service/service-shape1-2.webp" alt="img">
     </div>
     <div class="leaf-shape-animation" data-bg-src="url(assets/img/icon/leaf.svg)">
     </div>
@@ -39,22 +39,22 @@ Breadcumb
         <div class="gallery-container px-4 py-4 mb-4">
             <div class="gallery-item" data-index="1">
 
-                <img loading="lazy"  src="assets/images/gallery/convicity-2.jpg" alt="image" >
+                <img loading="lazy"  src="assets/images/gallery/convicity-2.webp" alt="image" >
             </div>
             <div class="gallery-item" data-index="2">
-                <img loading="lazy"  src="assets/images/gallery/convicity-1.jpg" alt="image">
+                <img loading="lazy"  src="assets/images/gallery/convicity-1.webp" alt="image">
             </div>
             <div class="gallery-item" data-index="3">
                 <img loading="lazy"  src="assets/images/gallery/226.webp" alt="image">
             </div>
             <div class="gallery-item" data-index="4">
-                <img loading="lazy"  src="assets/images/gallery/23.jpeg" alt="image">
+                <img loading="lazy"  src="assets/images/gallery/23.webp" alt="image">
             </div>
             <div class="gallery-item" data-index="5">
-                <img loading="lazy"  src="assets/images/gallery/24.jpeg" alt="image">
+                <img loading="lazy"  src="assets/images/gallery/24.webp" alt="image">
             </div>
             <div class="gallery-item" data-index="6">
-                <img loading="lazy"  src="assets/images/gallery/25.jpeg" alt="image">
+                <img loading="lazy"  src="assets/images/gallery/25.webp" alt="image">
             </div>
             <div class="gallery-item" data-index="7">
                 <img loading="lazy"  src="assets/images/gallery/villa-1.webp" alt="image">
@@ -63,7 +63,7 @@ Breadcumb
                 <img loading="lazy"  src="assets/images/gallery/27.webp" alt="image">
             </div>
             <div class="gallery-item" data-index="9">
-                <img loading="lazy"  src="assets/images/gallery/22.jpeg" alt="image">
+                <img loading="lazy"  src="assets/images/gallery/22.webp" alt="image">
             </div>
             <div class="gallery-item" data-index="10">
                 <img loading="lazy"  src="assets/images/gallery/villa-2.webp" alt="image">
